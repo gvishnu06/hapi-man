c190c7629840ec14ae1253f18acc53cc603e8b1d
vm13 <vishnu.gondlekar@in.tesco.com>
2015-04-17 00:03:09 +0530

    changing mongo connection uri

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var Mongoose = require('mongoose');
 var config = require('./config');
-Mongoose.connect('mongodb://' + config.database.host + '/' + config.database.db);
+Mongoose.connect('mongodb://ds061661.mongolab.com:61661/heroku_app35938487');
 var db = Mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', function callback() {
