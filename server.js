6863316affbe3ca752364fa833b974da0714a123
vm13 <vishnu.gondlekar@in.tesco.com>
2015-04-17 01:36:12 +0530

    uri change

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var boom = require('boom');
 // Create a server with a host and port
 var server = new Hapi.Server();
 server.connection({ 
-    host: 'localhost', 
+    host: process.env.HOST || 'localhost', 
     port: parseInt(process.env.PORT, 10) || 3000
 });
 
