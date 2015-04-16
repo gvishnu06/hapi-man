fff4d61379f941211ef9a29bb308a68375f738da
vm13 <vishnu.gondlekar@in.tesco.com>
2015-04-17 01:19:37 +0530

    uri change

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var boom = require('boom');
 // Create a server with a host and port
 var server = new Hapi.Server();
 server.connection({ 
-    host: 'https://mysterious-ocean-9932.herokuapp.com', 
+    host: 'localhost', 
     port: process.env.PORT
 });
 
